51fdabdcf607875570d6d9b94acb7b44a7205b5c
Sharion2023 <Sharion06@gmail.com>
2025-01-29 21:16:48 -0600

    JS started

---
+++ script.js
@@ -0,0 +1,80 @@
+console.log("Hello World!");
+
+const btn_open = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn_open.addEventListener("click", function () {
+  console.log("clicked!");
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn_open.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn_open.textContent = "Share a fact";
+  }
+});
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allCategories = CATEGORIES.map((el) => el.name);
+console.log(allCategories);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const factAges = initialFacts.map(
+  (el) => new Date().getFullYear() - el.createdIn
+);
+console.log(factAges);
+console.log("missed");
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+  return age;
+}
+
+console.log(calcFactAge(2009));
+
+const factAges1 = initialFacts.map((el) => calcFactAge(el.createdIn));
+console.log(factAges1);
