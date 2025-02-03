4d8baa30bdb9532f6bc71731f946d684c1bd543b
Sharion2023 <Sharion06@gmail.com>
2025-02-03 14:01:26 -0600

    DOM manipulation began

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-console.log("Hello World!");
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 
+//Selecting DOM elements
 const btn_open = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
 
+//Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+
+//Toggle form visibility
 btn_open.addEventListener("click", function () {
-  console.log("clicked!");
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
     btn_open.textContent = "Close";
@@ -13,6 +50,8 @@ btn_open.addEventListener("click", function () {
     btn_open.textContent = "Share a fact";
   }
 });
+
+/*
 
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
@@ -78,3 +117,4 @@ console.log(calcFactAge(2009));
 
 const factAges1 = initialFacts.map((el) => calcFactAge(el.createdIn));
 console.log(factAges1);
+*/
