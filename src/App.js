d76c707b284413da694ccfa13441c30069194cbe
Sharion2023 <Sharion06@gmail.com>
2025-02-17 21:49:46 -0600

    React app components started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import "./style.css";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      {/* Header*/}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Today I Learned Logo" />
+          <h1>Today I Learned!</h1>
+        </div>
+
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
+  );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact Form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>Fact List</section>;
 }
 
 export default App;
